f1112463faa2000614890ad80edc23af3f80402d
kruettlip <philippe.kruettli@gmx.ch>
2017-09-14 15:05:44 +0200

    First commit

---
+++ Mastermind/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			addGuessingButtons();
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void addGuessingButtons() {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
